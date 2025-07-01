53da3e0b097df52102a45cabd2cf96d03a55c3fa
Christian Häggström <christian.haggstrom@orexplore.com>
2025-07-01 10:44:42 +0200

    Change personnummer to name

--- id-provider-python/server.py
+++ id-provider-python/server.py
@@ -10,9 +10,9 @@ class EnrollReply(BaseModel):
 
 
 @app.post("/enroll")
-async def enroll(personnummer: str, pubkey: str) -> EnrollReply:
+async def enroll(name: str, pubkey: str) -> EnrollReply:
     "Register your secure token at the ID provider. Currently not using any crypto!"
-    statement = f"I verified that person '{personnummer}' possess a secure token with pubkey '{pubkey}'"
+    statement = f"I verified that person '{name}' possess a secure token with pubkey '{pubkey}'"
     return EnrollReply(signed_cert=statement, signature=str(uuid.uuid4()))
 
 
