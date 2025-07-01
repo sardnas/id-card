25bfd7b162a568e2214d6eed7034e5b39dbd39c9
Christian Häggström <christian.haggstrom@orexplore.com>
2025-07-01 11:40:40 +0200

    Allow CORS

--- id-provider-python/server.py
+++ id-provider-python/server.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uuid
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class EnrollReply(BaseModel):
     signed_cert: str
