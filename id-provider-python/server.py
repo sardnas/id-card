a177f35f265fe82a2231c67a57762512768d2a2c
Christian Häggström <christian.haggstrom@orexplore.com>
2025-07-01 14:07:53 +0200

    Implement revoke

--- id-provider-python/server.py
+++ id-provider-python/server.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uuid
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import timedelta
+
+# needed for any cluster connection
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster
+# needed for options -- cluster, timeout, SQL++ (N1QL) query, etc.
+from couchbase.options import (ClusterOptions, ClusterTimeoutOptions,
+                               QueryOptions)
+
+import couchbase.exceptions
+
+import os
+import json
+
 
 app = FastAPI()
 app.add_middleware(
@@ -27,4 +41,53 @@ async def enroll(name: str, pubkey: str) -> EnrollReply:
 @app.post("/revoke")
 async def revoke(pubkey: str) -> str:
     "Revoke a secure token. Currently doesn't check if the caller is allowed to."
+    database_revoke_pubkey(pubkey)
     return "OK"
+
+
+def setup_connection():
+    with open("credentials.json") as f:
+        creds = json.load(f)
+
+    endpoint = creds["host"]
+    username = creds["username"]
+    password = creds["password"]
+    bucket_name = creds["bucket"]
+
+    # Connect options - authentication
+    auth = PasswordAuthenticator(username, password)
+
+    # get a reference to our cluster
+    options = ClusterOptions(auth)
+    # Sets a pre-configured profile called "wan_development" to help avoid latency issues
+    # when accessing Capella from a different Wide Area Network
+    # or Availability Zone(e.g. your laptop).
+    options.apply_profile('wan_development')
+    cluster = Cluster(endpoint, options)
+
+    # Wait until the cluster is ready for use.
+    cluster.wait_until_ready(timedelta(seconds=5))
+
+    # get a reference to our bucket
+    cb = cluster.bucket(bucket_name)
+    return cb.scope("id-card-sc").collection("revoked-ids")
+
+
+print("Connecting...")
+collection = setup_connection()
+print("Connected")
+
+
+def database_revoke_pubkey(pubkey):
+    """Append an entry to the list in the database"""
+    try:
+        result = collection.get("revoked-ids")
+        revocation_list = result.content_as[list]
+    except couchbase.exceptions.DocumentNotFoundException:
+        revocation_list = []
+
+    print(f"{revocation_list=}")
+    revocation_list.append(pubkey)
+    collection.upsert("revoked-ids", revocation_list)
+
+    print(f"Uploaded {revocation_list=}")
